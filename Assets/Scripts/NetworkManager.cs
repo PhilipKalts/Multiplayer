1744338651115af8331c3f087c9653c11041741c
Philip Kalts <philkalts@gmail.com>
2021-09-05 09:16:30 +0300

    ui added

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using Photon.Pun;
+
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+	public static NetworkManager instance;
+
+	void Awake()
+	{
+		if (instance != null && instance != this) Destroy(gameObject);
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings();
+	}
+
+
+
+
+
+    public override void OnCreatedRoom()
+    {
+		print("Created Room: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public void JoinRoom(string roomName)
+	{
+		PhotonNetwork.JoinRoom(roomName);
+	}
+
+	[PunRPC]
+	public void ChangeScene (string sceneName)
+	{
+		PhotonNetwork.LoadLevel(sceneName);
+	}
+
+}
